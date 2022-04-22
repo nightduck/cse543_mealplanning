2ca527bf3016a3e34726351e6d45b59062009185
Colton Lewis <clewis01038@utexas.edu>
2022-04-22 10:37:26 -0500

    filled in a few remaining blanks with 0's and finalized parsing function

--- meals.py
+++ meals.py
@@ -4,6 +4,10 @@ import numpy as np
 # Meals = [{"name": "Pizza", "time": 5}, {}, {}]
 
 def CompileMealList():
+
+	numCols = 20
+	skipRows = 2
+
 	# simpler titles
 	titles = ["name",
 			  "tags",
@@ -27,10 +31,8 @@ def CompileMealList():
 			  "potassium"]
 
 	# skip rows 1 and 2
-	texts = np.loadtxt(open("meal.tsv", "rb"), dtype='str', delimiter="\t", usecols = (0,1))
-	# print(text)
-	# nums = np.loadtxt(open("meal.tsv", "rb"), delimiter="\t", usecols=(2,19)) use this when sheet full
-	nums = np.loadtxt(open("meal.tsv", "rb"), delimiter="\t", usecols=(range(2,13)))
+	texts = np.loadtxt(open("meal.tsv", "rb"), dtype='str', delimiter="\t", usecols = (0,1), skiprows=skipRows)
+	nums = np.loadtxt(open("meal.tsv", "rb"), delimiter="\t", usecols=(range(2,numCols)), skiprows=skipRows)  # use this when sheet full
 
 	meals = []
 	for text,num in zip(texts,nums):
@@ -39,12 +41,14 @@ def CompileMealList():
 
 		entry["name"] = text[0]
 		entry["tags"] = text[1]
-		for i in range(2,13):
+		for i in range(2,numCols):
+			print(i)
 			entry[titles[i]] = num[i-2]
 
 		meals.append(entry)
 
-	print(meals)
+	return meals
 
 # Main
-CompileMealList()
+meals = CompileMealList()
+print(meals)
